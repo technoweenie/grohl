63bef401c358822d86b95ad7c28e29fd6a5b5459
rick <technoweenie@gmail.com>
2013-09-02 13:05:23 -0700

    script/fmt

--- grohl.go
+++ grohl.go
@@ -3,33 +3,33 @@ package grohl
 var logger = NewLogger(nil)
 
 func Log(data map[string]interface{}) {
-  logger.Log(data)
+	logger.Log(data)
 }
 
 func NewContext(data map[string]interface{}) *IoLogger {
-  return logger.NewContext(data)
+	return logger.NewContext(data)
 }
 
 func AddContext(key string, value interface{}) {
-  logger.AddContext(key, value)
+	logger.AddContext(key, value)
 }
 
 func DeleteContext(key string) {
-  logger.DeleteContext(key)
+	logger.DeleteContext(key)
 }
 
 func NewTimer(context map[string]interface{}) *timer {
-  return logger.NewTimer(context)
+	return logger.NewTimer(context)
 }
 
 func SetLogger(l *IoLogger) {
-  logger = l
+	logger = l
 }
 
 func SetTimeUnit(unit string) {
-  logger.TimeUnit = unit
+	logger.TimeUnit = unit
 }
 
 func TimeUnit() string {
-  return logger.TimeUnit
+	return logger.TimeUnit
 }

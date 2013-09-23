97d233302266ee0b4c68f2d5a5521b1de317ca4d
risk danger olson <technoweenie@gmail.com>
2013-09-23 07:34:54 -0600

    remove the buffered logger.  it works well but isnt needed

--- loggers.go
+++ loggers.go
@@ -4,10 +4,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"os/signal"
-	"path/filepath"
-	"syscall"
-	"time"
 )
 
 // A really basic logger that builds lines and writes to any io.Writer.  This
@@ -47,126 +43,13 @@ func (l *ChannelLogger) Log(data Data) error {
 	return nil
 }
 
-type BufferedLogger struct {
-	Position   int
-	Upperbound int
-	AddTime    bool
-	Timeout    time.Duration
-	Writer     io.WriteCloser
-	buffer     []byte
-}
-
-func NewBufferedLogger(filename string, upperbound int) *BufferedLogger {
-	stdout := &nopcloser{os.Stdout}
-	var writer io.WriteCloser = stdout
-
-	if len(filename) > 0 {
-		err := os.MkdirAll(filepath.Dir(filename), 0744)
-		if err == nil {
-			writer = &bufferedFileWriter{filename, stdout}
-		} else {
-			fmt.Printf("Error opening log file %s: %s\n", filename, err.Error())
-		}
-	}
-
-	return &BufferedLogger{
-		Position:   0,
-		Upperbound: upperbound,
-		AddTime:    true,
-		Timeout:    1 * time.Minute,
-		Writer:     writer,
-		buffer:     make([]byte, upperbound),
-	}
-}
-
-func WriteBufferedLogs(logch chan Data, filename string, upperbound int) {
-	NewBufferedLogger(filename, upperbound).Watch(logch)
-}
-
-func (l *BufferedLogger) Log(data Data) {
-	line := l.BuildLog(data)
-	length := len(line)
-	if length > l.Upperbound {
-		l.Write(line)
-		return
-	}
-
-	if (length + l.Position) > l.Upperbound {
-		l.Flush()
-	}
-
-	copy(l.buffer[l.Position:], line)
-	l.Position += length
-}
-
-func (l *BufferedLogger) Write(data []byte) (int, error) {
-	return l.Writer.Write(data)
-}
-
-func (l *BufferedLogger) Watch(logch chan Data) {
-	sigch := make(chan os.Signal)
-	signal.Notify(sigch, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGTERM, syscall.SIGQUIT)
-
+func Watch(logger Logger, logch chan Data) {
 	for {
-		select {
-		case <-sigch:
-			l.Flush()
-		case data := <-logch:
-			if data != nil {
-				l.Log(data)
-			} else {
-				l.Flush()
-				return
-			}
-		case <-time.After(l.Timeout):
-			l.Flush()
+		data := <-logch
+		if data != nil {
+			logger.Log(data)
+		} else {
+			return
 		}
 	}
-}
-
-func (l *BufferedLogger) BuildLog(data Data) []byte {
-	return []byte(fmt.Sprintf("%s\n", BuildLog(data, l.AddTime)))
-}
-
-func (l *BufferedLogger) Flush() {
-	if l.Position == 0 {
-		return
-	}
-
-	l.Write(l.buffer[:l.Position])
-	l.Position = 0
-}
-
-type bufferedFileWriter struct {
-	Filename string
-	Fallback io.WriteCloser
-}
-
-func (w *bufferedFileWriter) Write(data []byte) (int, error) {
-	stream := w.openStream()
-	defer stream.Close()
-	return stream.Write(data)
-}
-
-func (w *bufferedFileWriter) Close() error {
-	return nil
-}
-
-func (w *bufferedFileWriter) openStream() io.WriteCloser {
-	file, err := os.OpenFile(w.Filename, os.O_RDWR|os.O_APPEND|os.O_CREATE|os.O_SYNC, 0644)
-	if err == nil {
-		return file
-	} else {
-		fmt.Printf("Error opening log file %s: %s\n", w.Filename, err.Error())
-	}
-
-	return w.Fallback
-}
-
-type nopcloser struct {
-	io.Writer
-}
-
-func (c *nopcloser) Close() error {
-	return nil
 }
--- loggers_test.go
+++ loggers_test.go
@@ -2,9 +2,6 @@ package grohl
 
 import (
 	"bytes"
-	"io/ioutil"
-	"os"
-	"strconv"
 	"strings"
 	"testing"
 )
@@ -35,86 +32,6 @@ func TestChannelLog(t *testing.T) {
 
 	if data["a"] != recv["a"] {
 		t.Errorf("Received: %s", recv)
-	}
-}
-
-func TestBufferedLog(t *testing.T) {
-	file, err := ioutil.TempFile("", "testing")
-	if err != nil {
-		t.Fatalf("Error opening tempfile: %s", err.Error())
-	}
-
-	defer file.Close()
-	defer os.Remove(file.Name())
-
-	logger := NewBufferedLogger(file.Name(), 10)
-	logger.AddTime = false
-
-	logger.Log(Data{"a": 1})
-	logger.Log(Data{"b": 2})
-
-	readFile := func() string {
-		by, err := ioutil.ReadAll(file)
-		if err != nil {
-			t.Fatalf("Error reading tempfile: %s", err.Error())
-			return ""
-		}
-		return string(by)
-	}
-
-	if actual := readFile(); len(actual) > 0 {
-		t.Errorf("expected empty string, got %s", actual)
-	}
-
-	logger.Log(Data{"c": 3})
-
-	expected := "a=1\nb=2\n"
-	if actual := readFile(); actual != expected {
-		t.Errorf("e: %s\na: %s", strconv.Quote(expected), strconv.Quote(actual))
-	}
-
-	expected = "c=3\n"
-	if actual := string(logger.buffer[:logger.Position]); actual != expected {
-		t.Errorf("e: %s\na: %s", strconv.Quote(expected), strconv.Quote(actual))
-	}
-}
-
-func TestBufferedLogFallback(t *testing.T) {
-	buf := bytes.NewBufferString("")
-	logger := NewBufferedLogger("", 10)
-	logger.AddTime = false
-	logger.Writer = &nopcloser{buf}
-
-	logger.Log(Data{"a": 1})
-	logger.Log(Data{"b": 2})
-
-	if actual := buf.String(); len(actual) > 0 {
-		t.Errorf("expected empty string, got %s", actual)
-	}
-
-	logger.Log(Data{"c": 3})
-
-	expected := "a=1\nb=2\n"
-	if actual := buf.String(); actual != expected {
-		t.Errorf("e: %s\na: %s", strconv.Quote(expected), strconv.Quote(actual))
-	}
-
-	expected = "c=3\n"
-	if actual := string(logger.buffer[:logger.Position]); actual != expected {
-		t.Errorf("e: %s\na: %s", strconv.Quote(expected), strconv.Quote(actual))
-	}
-}
-
-func TestBufferLongLine(t *testing.T) {
-	buf := bytes.NewBufferString("")
-	logger := NewBufferedLogger("", 1)
-	logger.AddTime = false
-	logger.Writer = &nopcloser{buf}
-
-	logger.Log(Data{"a": 1})
-	expected := "a=1\n"
-	if actual := buf.String(); actual != expected {
-		t.Errorf("e: %s\na: %s", strconv.Quote(expected), strconv.Quote(actual))
 	}
 }
 

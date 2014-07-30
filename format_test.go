ffe6228288d7a53f0bfccb8eecb0425feb27ce74
risk danger olson <technoweenie@gmail.com>
2014-07-30 05:22:15 -0600

    restructure format tests
    Hashes are no longer ordered in Go 1.3.  This asserts
    formatting by comparing against an array of string key/value
    pairs.

--- format_test.go
+++ format_test.go
@@ -14,96 +14,71 @@ type ExampleStruct struct {
 	Value interface{}
 }
 
-var examples = map[string]Data{
-	"fn=string test=hi": Data{
-		"fn": "string", "test": "hi",
-	},
-	`fn=stringspace test="a b"`: Data{
-		"fn": "stringspace", "test": "a b",
-	},
-	`fn=stringslasher test="slasher \\\\"`: Data{
-		"fn": "stringslasher", "test": `slasher \\`,
-	},
-	`fn=stringeqspace test="x=4, y=10"`: Data{
-		"fn": "stringeqspace", "test": "x=4, y=10",
-	},
-	`fn=stringeq test="x=4,y=10"`: Data{
-		"fn": "stringeq", "test": "x=4,y=10",
-	},
-	`fn=stringspace test="hello world"`: Data{
-		"fn": "stringspace", "test": "hello world",
-	},
-	`fn=stringbothquotes test="echo 'hello' \"world\""`: Data{
-		"fn": "stringbothquotes", "test": `echo 'hello' "world"`,
-	},
-	`fn=stringsinglequotes test="a 'a'"`: Data{
-		"fn": "stringsinglequotes", "test": `a 'a'`,
-	},
-	`fn=stringdoublequotes test='echo "hello"'`: Data{
-		"fn": "stringdoublequotes", "test": `echo "hello"`,
-	},
-	`fn=stringbothquotesnospace test='a"`: Data{
-		"fn": "stringbothquotesnospace", "test": `'a"`,
-	},
-	"fn=emptystring test=nil": Data{
-		"fn": "emptystring", "test": "",
-	},
-	"fn=int test=1": Data{
-		"fn": "int", "test": int(1),
-	},
-	"fn=int8 test=1": Data{
-		"fn": "int8", "test": int8(1),
-	},
-	"fn=int16 test=1": Data{
-		"fn": "int16", "test": int16(1),
-	},
-	"fn=int32 test=1": Data{
-		"fn": "int32", "test": int32(1),
-	},
-	"fn=int64 test=1": Data{
-		"fn": "int64", "test": int64(1),
-	},
-	"fn=uint test=1": Data{
-		"fn": "uint", "test": uint(1),
-	},
-	"fn=uint8 test=1": Data{
-		"fn": "uint8", "test": uint8(1),
-	},
-	"fn=uint16 test=1": Data{
-		"fn": "uint16", "test": uint16(1),
-	},
-	"fn=uint32 test=1": Data{
-		"fn": "uint32", "test": uint32(1),
-	},
-	"fn=uint64 test=1": Data{
-		"fn": "uint64", "test": uint64(1),
-	},
-	"fn=float test=1.000": Data{
-		"fn": "float", "test": float32(1.0),
-	},
-	"fn=bool test=true": Data{
-		"fn": "bool", "test": true,
-	},
-	"fn=nil test=nil": Data{
-		"fn": "nil", "test": nil,
-	},
-	"fn=time test=2000-01-02T03:04:05+0000": Data{
-		"fn": "time", "test": exampleTime,
-	},
-	`fn=error test="error message"`: Data{
-		"fn": "error", "test": exampleError,
-	},
-	`fn=slice test="[86 87 88]"`: Data{
-		"fn": "slice", "test": []byte{86, 87, 88},
-	},
-	`fn=struct test={Value:testing123}`: Data{
-		"fn": "struct", "test": ExampleStruct{Value: "testing123"},
-	},
+var actuals = []Data{
+	Data{"fn": "string", "test": "hi"},
+	Data{"fn": "stringspace", "test": "a b"},
+	Data{"fn": "stringslasher", "test": `slasher \\`},
+	Data{"fn": "stringeqspace", "test": "x=4, y=10"},
+	Data{"fn": "stringeq", "test": "x=4,y=10"},
+	Data{"fn": "stringspace", "test": "hello world"},
+	Data{"fn": "stringbothquotes", "test": `echo 'hello' "world"`},
+	Data{"fn": "stringsinglequotes", "test": `a 'a'`},
+	Data{"fn": "stringdoublequotes", "test": `echo "hello"`},
+	Data{"fn": "stringbothquotesnospace", "test": `'a"`},
+	Data{"fn": "emptystring", "test": ""},
+	Data{"fn": "int", "test": int(1)},
+	Data{"fn": "int8", "test": int8(1)},
+	Data{"fn": "int16", "test": int16(1)},
+	Data{"fn": "int32", "test": int32(1)},
+	Data{"fn": "int64", "test": int64(1)},
+	Data{"fn": "uint", "test": uint(1)},
+	Data{"fn": "uint8", "test": uint8(1)},
+	Data{"fn": "uint16", "test": uint16(1)},
+	Data{"fn": "uint32", "test": uint32(1)},
+	Data{"fn": "uint64", "test": uint64(1)},
+	Data{"fn": "float", "test": float32(1.0)},
+	Data{"fn": "bool", "test": true},
+	Data{"fn": "nil", "test": nil},
+	Data{"fn": "time", "test": exampleTime},
+	Data{"fn": "error", "test": exampleError},
+	Data{"fn": "slice", "test": []byte{86, 87, 88}},
+	Data{"fn": "struct", "test": ExampleStruct{Value: "testing123"}},
+}
+
+var expectations = [][]string{
+	[]string{"fn=string", "test=hi"},
+	[]string{"fn=stringspace", `test="a b"`},
+	[]string{`fn=stringslasher`, `test="slasher \\\\"`},
+	[]string{`fn=stringeqspace`, `test="x=4, y=10"`},
+	[]string{`fn=stringeq`, `test="x=4,y=10"`},
+	[]string{`fn=stringspace`, `test="hello world"`},
+	[]string{`fn=stringbothquotes`, `test="echo 'hello' \"world\""`},
+	[]string{`fn=stringsinglequotes`, `test="a 'a'"`},
+	[]string{`fn=stringdoublequotes`, `test='echo "hello"'`},
+	[]string{`fn=stringbothquotesnospace`, `test='a"`},
+	[]string{"fn=emptystring", "test=nil"},
+	[]string{"fn=int", "test=1"},
+	[]string{"fn=int8", "test=1"},
+	[]string{"fn=int16", "test=1"},
+	[]string{"fn=int32", "test=1"},
+	[]string{"fn=int64", "test=1"},
+	[]string{"fn=uint", "test=1"},
+	[]string{"fn=uint8", "test=1"},
+	[]string{"fn=uint16", "test=1"},
+	[]string{"fn=uint32", "test=1"},
+	[]string{"fn=uint64", "test=1"},
+	[]string{"fn=float", "test=1.000"},
+	[]string{"fn=bool", "test=true"},
+	[]string{"fn=nil", "test=nil"},
+	[]string{"fn=time", "test=2000-01-02T03:04:05+0000"},
+	[]string{`fn=error`, `test="error message"`},
+	[]string{`fn=slice`, `test="[86 87 88]"`},
+	[]string{`fn=struct`, `test={Value:testing123}`},
 }
 
 func TestFormat(t *testing.T) {
-	for expected, data := range examples {
-		AssertData(t, expected, data)
+	for i, actual := range actuals {
+		AssertData(t, actual, expectations[i])
 	}
 }
 
@@ -118,13 +93,19 @@ func TestFormatWithTime(t *testing.T) {
 	}
 }
 
-func AssertLog(t *testing.T, expected string, ctx *Context) {
-	AssertData(t, expected, ctx.Merge(nil))
+func AssertLog(t *testing.T, ctx *Context, expected []string) {
+	AssertData(t, ctx.Merge(nil), expected)
 }
 
-func AssertData(t *testing.T, expected string, data Data) {
-	if actual := BuildLog(data, false); expected != actual {
-		t.Errorf("Expected %s\nfrom: %v\nGot: %s", expected, data, actual)
+func AssertData(t *testing.T, data Data, expected []string) {
+	pairs, line := buildLogMap(data)
+	for _, pair := range expected {
+		if _, ok := pairs[pair]; !ok {
+			t.Errorf("Expected pair '%s' in %s", pair, line)
+		}
+	}
+	if expectedLen := len(expected); expectedLen != len(pairs) {
+		t.Errorf("Expected %d pairs in %s", expectedLen, line)
 	}
 }
 
@@ -132,4 +113,13 @@ func AssertString(t *testing.T, expected, actual string) {
 	if expected != actual {
 		t.Errorf("Expected %s\nGot: %s", expected, actual)
 	}
+}
+
+func buildLogMap(d Data) (map[string]bool, string) {
+	m := make(map[string]bool)
+	parts := BuildLogParts(d, false)
+	for _, pair := range parts {
+		m[pair] = true
+	}
+	return m, strings.Join(parts, space)
 }
